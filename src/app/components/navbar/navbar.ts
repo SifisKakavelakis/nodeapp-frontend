05f51f61749c38e6c76f3aa5f3adbe742e7cab61
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-05-04 20:55:44 +0300

    feat: add login, logout

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { UserService } from '../../shared/services/user.service';
+import { MatIconModule, MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIcon],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  userService = inject(UserService);
+  user = this.userService.user;
+
+  logout(){
+    this.userService.logoutUser();
+  }
+}
