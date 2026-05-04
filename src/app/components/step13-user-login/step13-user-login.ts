a3e51fc5a6907c2c5d4a5c0d508cb022e91f6c3b
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-05-04 20:03:58 +0300

    fead: add user login and signal variable

--- src/app/components/step13-user-login/step13-user-login.ts
+++ src/app/components/step13-user-login/step13-user-login.ts
@@ -31,8 +31,13 @@ export class Step13UserLogin {
         console.log(response);
         const access_token = response.token;
         localStorage.setItem('access_token', access_token)
-        const decodedToken=jwtDecode(access_token);
+        const decodedToken=jwtDecode(access_token) as unknown as LoggedInUser;
         console.log("DECODED TOKEN>>", decodedToken);
+        this.userService.user.set({
+          username: decodedToken.username,
+          email: decodedToken.email,
+          roles: decodedToken.roles
+        })
       },
       error: (error) => {
         console.log(error);
